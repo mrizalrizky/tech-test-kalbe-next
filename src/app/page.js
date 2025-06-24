5e2998ac40a0236d5a242e9234b0fbc20395c4d7
mrizalrizky <mrizalrn@gmail.com>
2025-06-24 15:40:39 +0700

    fix: dashboard

--- src/app/page.js
+++ src/app/page.js
@@ -35,9 +35,7 @@ export default function ProductDashboardPage() {
     <main className="min-h-screen bg-gray-50 px-4 py-6">
       <div className="max-w-6xl mx-auto">
         <header className="flex justify-between items-center mb-8">
-          <h1 className="text-2xl font-bold text-gray-800">
-            Selamat datang, {"Pengguna"} 👋
-          </h1>
+          <h1 className="text-2xl font-bold text-gray-800">Selamat datang!</h1>
           <button
             onClick={() => handleLogout}
             className="px-4 py-2 bg-red-500 text-white rounded-xl hover:bg-red-600 transition"
